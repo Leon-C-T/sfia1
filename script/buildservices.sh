13f706d6de195d2e489be13efeed107de4d6ea75
Leon-C-T <leont1280@gmail.com>
2020-03-17 14:46:26 +0000

    Major change to jenkinsfile and call structure

--- script/buildservices.sh
+++ script/buildservices.sh
@@ -1,5 +1,5 @@
 sudo systemctl stop mintbooking.service
-sudo cp "/var/lib/jenkins/workspace/sfia1/script/mintbooking.service" /etc/systemd/system
+sudo cp /var/lib/jenkins/workspace/sfia1/script/mintbooking.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable mintbooking.service
 sudo systemctl start mintbooking.service
