d2dbb767efc4f3e4ceb9e93b789a91955e1f409e
Leon-C-T <leont1280@gmail.com>
2020-03-17 11:53:51 +0000

    quick change to testing.py

--- test/testing.py
+++ test/testing.py
@@ -2,5 +2,5 @@ import urllib3
 
 def test_url():
     http = urllib3.PoolManager() 
-    r = http.request('GET', 'http://127.0.0.1:5000/') # can use post method too ## or ip address of the website:5000 when its running on another machine
+    r = http.request('GET', 'http://http://35.234.133.4:5000/') # can use post method too ## or ip address of the website:5000 when its running on another machine
     assert 200 == r.status  #200 = successful connection
