8a21dfb75bfafaec389396d6dddbf9a4379318ac
Leon-C-T <leont1280@gmail.com>
2020-03-17 11:15:02 +0000

    quick change to testing.py

--- test/testing.py
+++ test/testing.py
@@ -2,5 +2,5 @@ import urllib3
 
 def test_url():
     http = urllib3.PoolManager() 
-    r = http.request('GET', 'http://mintbookings.ddns.net') # can use post method too
+    r = http.request('GET', 'http://localhost:5000/') # can use post method too ## or ip address of the website:5000 when its running on another machine
     assert 200 == r.status  #200 = successful connection
