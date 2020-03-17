7e0f53162a5f224d250387ccc46e0783dceb6ee5
Leon-C-T <leont1280@gmail.com>
2020-03-17 11:59:17 +0000

    quick change to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -2,7 +2,6 @@
 
 source venv/bin/activate
 
-
 python3 -m pip install flask
 
 python3 -m pip install flask_mysqldb
@@ -13,7 +12,7 @@ python3 -m pip install urllib3
 
 python3 -m pip install pytest-base-url
 
-python3 -m pip install coverage
+pip3 install coverage
 
 source ~/.bashrc
 
