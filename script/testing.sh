087c8ab21dfb1971d5f340ddfc4535ffb5ad8859
Leon-C-T <leont1280@gmail.com>
2020-03-19 15:01:18 +0000

    change to testing.sh

--- script/testing.sh
+++ script/testing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source ~/.bashrc
 pip3 show coverage
-python3 -m coverage run -m pytest /var/lib/jenkins/workspace/sfia1/test/testing.py
-python3 -m coverage report -m
+python3 -m coverage run -m --source=. pytest ./test/testing.py
+python3 -m coverage report ./test/testing.py 
+# python3 -m coverage run -m pytest /var/lib/jenkins/workspace/sfia1/test/testing.py
+# python3 -m coverage report
