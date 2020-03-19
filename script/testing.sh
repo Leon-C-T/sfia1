c547c1dc9edc0143ee3d3e18095eee45f9006b60
Leon-C-T <leont1280@gmail.com>
2020-03-19 12:40:18 +0000

    Added testing script

--- script/testing.sh
+++ script/testing.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source ~/.bashrc
-python3 -m pytest /var/lib/jenkins/workspace/sfia1/test/testing.py
 pip3 show coverage
 python3 -m coverage run -m pytest /var/lib/jenkins/workspace/sfia1/test/testing.py
 python3 -m coverage report -m
