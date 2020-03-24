9f8a778b17ac94b76b46fdf0326d2b9071f1b039
Leon-C-T <leont1280@gmail.com>
2020-03-24 16:55:11 +0000

    for new jenkins server

--- script/buildservices.sh
+++ script/buildservices.sh
@@ -1,5 +1,5 @@
-systemctl stop mintbooking.service
-cp /var/jenkins_home/workspace/sfia1/script/mintbooking.service /etc/systemd/system
-systemctl daemon-reload
-systemctl enable mintbooking.service
-systemctl start mintbooking.service
+sudo systemctl stop mintbooking.service
+sudo cp /var/lib/jenkins/workspace/sfia1/script/mintbooking.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable mintbooking.service
+sudo systemctl start mintbooking.service
